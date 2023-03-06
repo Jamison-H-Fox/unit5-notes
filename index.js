12d9347aa9a96580bbab41ae4dacc428d534c2b6
Jamison Fox <fox.jamison@outlook.com>
2023-03-06 16:02:22 -0700

    linked list practice completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Valid Bracket Sequence
 const sequence = '('
 
-function solution(sequence) {
+function validBracketSequence(sequence) {
     const stack = [];
 
     const match_map = {
@@ -27,3 +27,51 @@ function solution(sequence) {
     return stack.length; // this works because stack.length will evalute to false if == 0, or truthy if non-0
 }
 
+// Node list stuff
+
+// Singly-linked lists are already defined with this interface:
+function ListNode(x) {
+  this.value = x;
+  this.next = null;
+}
+//
+
+const head = [1, 2, 3];
+const value = 99;
+const index = 3;
+
+function nodeListStuff(head, value, index) {
+    let n = new ListNode(value);
+
+    if (head == null) {
+        return n;
+    }
+
+    if (index <= 0) {
+        n.next = head;
+        return n;
+    }
+    
+    let prev = head;
+    let cur = head.next;
+    let cur_index = 1;
+
+    while (cur != null) {
+        if(cur_index == index) {
+            prev.next = n;
+            n.next = cur;
+            
+            return head;
+        }
+        
+        prev = prev.next;
+        cur = cur.next;
+        cur_index++;
+    }
+    
+    prev.next = n;
+    
+    return head;
+}
+
+console.log(nodeListStuff(head, value, index))
